f84e53a8b4e2a621339957cb9eda7c41f85ca1c6
Adrien Maranville <Adrien@Maranville.IO>
2019-02-24 22:13:09 -0700

    added json config

--- config/database.js
+++ config/database.js
@@ -1,3 +1,9 @@
 module.exports = {
-  mongoDbUrl: 'mongodb://localhost:27017/cms',
+  // mongoDbUrl: 'mongodb://localhost:27017/cms',
+  mongoDbUrl: 'mongodb://cmsRoot:ge4jt0js7fuVBW7r@node-js-cms-shard-00-00-iznbt.mongodb.net:27017,' +
+    'node-js-cms-shard-00-01-iznbt.mongodb.net:27017,' +
+    'node-js-cms-shard-00-02-iznbt.mongodb.net:27017/cms?ssl=true&' +
+    'replicaSet=node-js-cms-shard-0&' +
+    'authSource=admin&' +
+    'retryWrites=true',
 };
